c31dff10799b86ef9e0950c547786d3136f3b647
pukhraj1999 <pukhrajkhinda@gmail.com>
2021-10-22 23:28:19 +0530

    Social initiation

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import "./styles/Home.css";
+import Logo from "./assets/logo.png";
+import { NavLink } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <div className="desktop">
+        <div className="social__box">
+          <div className="social__content">
+            <img src={Logo} alt="Social logo" />
+          </div>
+          <div className="social__tabs">
+            <NavLink to="/">SERVICES</NavLink>
+            <NavLink to="/">OUR WORK</NavLink>
+            <NavLink style={{ color: "black" }} to="/">
+              CONTACT US
+            </NavLink>
+            <NavLink style={{ color: "black" }} to="/">
+              ABOUT US
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div className="mobile">
+        <h1>Mobile Version</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
