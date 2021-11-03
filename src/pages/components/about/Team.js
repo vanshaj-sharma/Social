f2aaf3c3f6c10346000dde92d782c8e1f89606f9
Rasleen <rasleenbedi04@gmail.com>
2021-11-03 14:18:44 +0530

    AboutUs=> Team.js grid system added

--- src/pages/components/about/Team.js
+++ src/pages/components/about/Team.js
@@ -4,9 +4,9 @@ import "./styles/Team.css";
 function Photos() {
   return (
     <>
-      <div className="Photos">
+      <div className="Photos col-md-3">
         <div className="Photos__content"></div>
-        <div className="Photos__title d-flex justify-content-center">
+        <div className="Photos__title">
           <h6>name1</h6>
         </div>
       </div>
@@ -18,12 +18,12 @@ function Team() {
   return (
     <>
       <div className="container">
-        <div className="d-flex">
+        <div className="row">
           <Photos />
           <Photos />
           <Photos />
         </div>
-        <div className="d-flex">
+        <div className="row">
           <Photos />
           <Photos />
           <Photos />
