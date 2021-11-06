7a551d833947cbfa95b388e083160997b9bc7fd0
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2021-11-06 12:41:10 +0530

    AboutUs page with unfinished animations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Team from "./components/about/Team.js";
+import We from "./components/about/We.js";
 import "./styles/About.css";
 
 function About() {
+  const [select, setSelected] = useState("0");
+  // const activate = (index) => {
+  //   let toggler = document.querySelectorAll(".content");
+  //   toggler[index].classList.toggle("content__active");
+  //   for (let i = 0; i < 3; i++) {
+  //     if (i === index) continue;
+  //     toggler[i].classList.remove("content__active");
+  //   }
+  // };
+  // useEffect(() => {
+  //   activate(0);
+  // }, []);
   return (
     <>
       <div className="desktop">
@@ -17,41 +30,46 @@ function About() {
           </div>
           <div className="container-fluid">
             <div className="row">
-              <div className="col-7 col-lg-9 about__left ">
-                <p>
-                  We are a digital agency. We deal with almost everything
-                  starting from influencer marketing, designing, social media,
-                  photography campaigns, photo and video editing, website
-                  development to complete digital marketing.
-                </p>
-                <p>
-                  You see something that has to be done for your brand on the
-                  internet? We can make it happen.
-                </p>
-                <p>
-                  From planning out your social media to curating content for
-                  it, we got an arsenal of services so you don’t have to go out
-                  looking for different things and bring them together.
-                </p>
-                <p>
-                  With as vision of simplicity, we started as an organisation
-                  that does what is necessary and leave out anything that won’t
-                  bring about a difference. This lets us keep our vision for
-                  each brand straight without clutter or confusion.
-                </p>
-                <hr />
-                <div id="team">
-                  <Team />
-                </div>
+              <div className="col-7 col-lg-9 about__left">
+                {select === "0" && (
+                  <div className="content__active">
+                    <We />
+                  </div>
+                )}
+                {select === "1" && (
+                  <div className="content__active">
+                    <Team />
+                  </div>
+                )}
+                {select === "2" && <h1 className="content__active">hello3</h1>}
               </div>
 
               <div className="col-5 col-lg-3 d-flex flex-column  about__right">
-                <button className="button">
-                  <a href="#team">TEAM</a>
+                <button
+                  className="button"
+                  onClick={() => {
+                    setSelected("0");
+                  }}
+                >
+                  <a>WHO ARE WE?</a>
                 </button>
 
-                <button className="button">
-                  <a href="#clientle">CLIENTLE</a>
+                <button
+                  className="button"
+                  onClick={() => {
+                    setSelected("1");
+                  }}
+                >
+                  <a>OUR TEAM</a>
+                </button>
+
+                <button
+                  className="button"
+                  onClick={() => {
+                    setSelected("2");
+                  }}
+                >
+                  <a>CLIENTLE</a>
                 </button>
               </div>
             </div>
