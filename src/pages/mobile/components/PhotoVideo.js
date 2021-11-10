7658a1c3d59a2b369b1de8b8c4decccb3d808a58
Rasleen <rasleenbedi04@gmail.com>
2021-11-10 23:54:51 +0530

    Premature mbservices components

---
+++ src/pages/mobile/components/PhotoVideo.js
@@ -0,0 +1,35 @@
+function mbPhotoVideo() {
+  return (
+    <>
+      <div>
+        <h1 className="s_title">SERVICES</h1>
+        <div className="s_brand">
+          <h1>Photo and Video</h1>
+          <p>
+            Photography is an art of capturing the what we see on to a film/
+            digital reel. It’s not just pointing the camera at something and
+            pressing that button. It takes years to learn and even more to get
+            great at it and we have been doing it for even more. We mainly excel
+            in Commercial Photo and Video shoots so we can show your customers
+            what your brand stands for. What’s that story behind every product
+            of your brand that makes it stand out in the market, so they’re not
+            just buying your products but their stories along with it. Here are
+            things that we do:
+          </p>
+          <ul>
+            <li>Campaign Shoots</li>
+            <li>Organising the campaigns</li>
+            <li>Concept shoots</li>
+            <li>E-Commerce Photoshoots</li>
+            <li>Music Videos</li>
+            <li>Reels </li>
+            <li>Youtube Videos </li>
+            <li>Interviews </li>
+            <li>Podcasts </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+export default mbPhotoVideo;
