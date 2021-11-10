b50d4c554b9ceaccb5c2ab7e9fd3dbb22279e523
Pukhraj <pukhrajkhinda@gmail.com>
2021-11-10 13:20:37 +0530

    mobile->Home

--- src/pages/mobile/mbHome.js
+++ src/pages/mobile/mbHome.js
@@ -1,8 +1,26 @@
 import "./styles/mbHome.css";
+import Logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 function mbHome() {
   return (
     <>
-      <h1 className="text-light">Hello</h1>
+      <div className="mbHome__box">
+        <NavLink className="top__left" to="/sevices">
+          CONTACT US
+        </NavLink>
+        <NavLink className="top__right" to="/about">
+          ABOUT US
+        </NavLink>
+        <NavLink className="bottom__left" to="/services">
+          SERVICES
+        </NavLink>
+        <NavLink className="bottom__right" to="/work">
+          WORK
+        </NavLink>
+        <div className="mbHome__content">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
     </>
   );
 }
